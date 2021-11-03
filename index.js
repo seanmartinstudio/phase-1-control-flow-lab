8d960b325d00541bfb902036dda8412a1edca3f1
Sean Martin <sean@seanmartinstudio.com>
2021-11-03 14:37:52 -0700

    commit tree 01380fc2e822b1eed129dfe4bca975d2cc8622fd

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+//
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+  let price;
+  if (distance <= 400) {
+    price = "This one is on me!";
+  } else if (distance > 2500) {
+    price = "No can do.";
+  } else if (distance > 2000) {
+    price = "I will gladly take your thirty bucks.";
+   }
+   return price;
+  }
+  scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//
+
+function ternaryCheckCity(city) {
+  let message;
+  message = city === "NYC" ? ("Ok, sounds good.") : ("No go.");
+  return message;
 }
+ternaryCheckCity("NYC");
+
+//
+
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case "not as generous":
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye."
+      break;
+  }
+  return message;
+}
+switchOnCharmFromTip("tip is generous");
